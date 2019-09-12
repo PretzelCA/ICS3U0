8956658eca9563bbc2119e0ca3f28b12fa289370
Skye Viau <skye.viau@gmail.com>
2019-09-12 15:55:12 -0400

    more org

---
+++ Chapter 2/c2l1p3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class c2p3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your Gym, Computer, English, and Math marks. >");
+        String[] marks = input.nextLine().split(" ");
+
+        System.out.println("Your average is " + ((Integer.parseInt(marks[0]) + Integer.parseInt(marks[1]) + Integer.parseInt(marks[2]) + Integer.parseInt(marks[3])) / 4));
+    }
+}
