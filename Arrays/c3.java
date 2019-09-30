659fe44333912f71296e507f027df8d87f1de563
Skye Viau <skye.viau@gmail.com>
2019-09-30 00:22:25 -0400

    Finish challenge 3

--- Arrays/c3.java
+++ Arrays/c3.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class c3 {
     public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
 
+        int highestIQ = 0;
+
+        String[] names = new String[6];
+        int[] iq = new int[6];
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("insert name " + (i + 1) + " here > ");
+            names[i] = input.nextLine();
+            iq[i] = rand.nextInt((160 - 60)) + 60;
+        }
+
+        for (int i = 0; i < names.length; i++) {
+            if(iq[i] > iq[highestIQ]) {
+                highestIQ = i;
+            }
+        }
+
+        System.out.println(names[highestIQ] + " has the highest IQ with an IQ of " + iq[highestIQ]);
     }
 }
---
+++ Chapter 2/c4.java
@@ -0,0 +1,5 @@
+public class c4 {
+    public static void main(String[] args) {
+        
+    }
+}
