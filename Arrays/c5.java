837c6c1ac07eef8504b1d45e82d4bca5008cdbf5
Skye Viau <skye.viau@gmail.com>
2020-01-27 20:12:15 -0500

    Complete Arrays C5

--- Arrays/c5.java
+++ Arrays/c5.java
@@ -1,5 +1,30 @@
+import java.util.ArrayList;
+
 public class c5 {
     public static void main(String[] args) {
+        ArrayList<Integer> rnums = new ArrayList<Integer>();
 
+        int numsTotal = 0;
+        int i = 0;
+        int totalOdds = 0;
+
+        do {
+            rnums.add((int)(Math.random()*10+1));
+            numsTotal+=rnums.get(i);
+            System.out.println(rnums.get(i));
+            i++;
+        } while (numsTotal < 100);
+
+        System.out.println("It tooks " + i + " iterations");
+
+        for (int j = 0; j < i; j++) {
+            if (rnums.get(j) % 2 == 0) {
+                System.out.println(rnums.get(j));
+            } else {
+                totalOdds++;
+            }
+        }
+
+        System.out.println("There are " + totalOdds + " odd numbers");
     }
 }
---
+++ Arrays/c6.java
@@ -0,0 +1,5 @@
+public class c6 {
+    public static void main(String[] args) {
+        
+    }
+}
