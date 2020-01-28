e807f1d09ac78ceb28127bb7130838edc2e94b4a
Skye Viau <skye.viau@gmail.com>
2020-01-28 08:04:45 -0500

    Written exam prep morning

---
+++ test/gonnaHaveAGreatTime.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class gonnaHaveAGreatTime {
+    public static void main(String[] args) {
+        String studentList[][] = new String[4][20];
+        int studentPhone [] = new int[69];
+        ArrayList<Integer> specialNumbers = new ArrayList<Integer>();
+
+        specialNumbers.add((int) (Math.random() * (420 - 69) + 1) - 69);
+        specialNumbers.add((int) (Math.random() * (420 - 69) + 1) - 69);
+        specialNumbers.add((int) (Math.random() * (420 - 69) + 1) - 69);
+        specialNumbers.add((int) (Math.random() * (420 - 69) + 1) - 69);
+
+        System.out.println(specialNumbers.get(2));
+        System.out.println(specialNumbers.size());
+
+        studentPhone[34] = 911;
+
+        studentList[2][12] = "AAAAA I HAVE THE EXAM IN AN HOUR LMAOOO CAN I REMEMBER SHIT IDK LETS SEE";
+
+        System.out.println("Debug here :)");
+    }
+}
